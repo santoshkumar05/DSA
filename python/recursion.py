df69f670b74442d246b09b877784b29eae0bdb48
santoshkumar05 <139670459+santoshkumar05@users.noreply.github.com>
2025-03-08 01:28:39 +0000

    add

--- python/recursion.py
+++ python/recursion.py
@@ -1,6 +1,25 @@
 # recursion - a function call by it self is called recursion.
 # stack over flow - when a function runs infinte times until the stack is full is called the stack over flow.
-def func():
-    print(1)
-    func()
-func()
+def fact(n):
+   if n==0:
+    return 1;
+   else:
+    return n*fact(n-1)
+def sum(n):
+   if n==0:
+    return 0;
+   else:
+    return n+sum(n-1)
+
+def num(n):
+ 
+    if n==0:
+        return 0;
+    else:
+        print(9-n+1)
+        return num(n-1)
+    
+print(sum(3))
+print(fact(4))
+num(9)
+    
